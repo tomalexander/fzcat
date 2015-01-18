187a9da59539a03385216d5e4225f0703f161e8f
Tom Alexander <craftkiller@gmail.com>
2015-01-18 17:20:55 -0500

    switched to single threaded

--- src/main.cpp
+++ src/main.cpp
@@ -8,33 +8,18 @@
 #include <unistd.h>
 #include "decompressor.h"
 
-#define NUM_THREADS 4
-
 /**
  * Decompress a full file at path using buffer_size bytes for a buffer
  */
-std::unique_ptr<std::stringstream> full_file(const long buffer_size, const char* path)
+void full_file(const long buffer_size, const char* path)
 {
-    std::unique_ptr<std::stringstream> out = std::make_unique<std::stringstream>();
     std::unique_ptr<char[]> buffer(new char[buffer_size]);
     decompressor dec(path);
     for (size_t read = dec.read(buffer.get(), buffer_size);
          read != 0;
          read = dec.read(buffer.get(), buffer_size))
     {
-        out->write(buffer.get(), read);
-    }
-    return std::move(out);
-}
-
-/**
- * Print out all the stringstream buffers in order
- */
-void print(std::vector<std::future<std::unique_ptr<std::stringstream> > > & results)
-{
-    for (std::future<std::unique_ptr<std::stringstream> > & out : results)
-    {
-        std::cout << out.get()->rdbuf();
+        std::cout.write(buffer.get(), read);
     }
 }
 
@@ -44,17 +29,9 @@ void print(std::vector<std::future<std::unique_ptr<std::stringstream> > > & resu
 int main(int argc, char** argv)
 {
     size_t buffer_size = std::max(2048, std::min(getpagesize(), 16384));
-    std::vector<std::future<std::unique_ptr<std::stringstream> > > results;
-    results.reserve(argc - 1);
     for (int i = 1; i < argc; ++i)
     {
-        results.push_back(std::async(std::launch::async, full_file, buffer_size, argv[i]));
-        if (i % NUM_THREADS == 0)
-        {
-            print(results);
-            results.clear();
-        }
+        full_file(buffer_size, argv[i]);
     }
-    print(results);
     return 0;
 }
